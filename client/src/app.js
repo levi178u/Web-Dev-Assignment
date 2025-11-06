3646684f085798bbebc7385e6e5853060208ed5a
Anshuman Panda <ichuk0078@gmail.com>
2025-11-06 20:26:20 +0530

    chore: added the client app engine

---
+++ client/src/app.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import LostItems from "./components/LostItem";
+import FoundItems from "./components/FoundItems";
+import Home from "./components/Home";
+import ItemPage from "./components/ItemPage";
+import LostItem from "./components/Lost_item";
+import MyListings from "./components/MyListings";
+import Layout from "./layout"; 
+window.OneSignal = window.OneSignal || [];
+const OneSignal = window.OneSignal;
+function App() {
+ 
+  
+  return (
+      <BrowserRouter>
+          <Layout>
+          <Routes>
+
+          <Route path="/" element={<Home />}  />
+          <Route path="/log-in" element={<Login/>} />
+          <Route path="/sign-up" element={<Signup/>} />
+          <Route path="/lostitems" element={<LostItems/>} />
+          <Route path="/founditems" element={<FoundItems/>} />
+          <Route path="/postitem" element={<LostItem/>} />
+          <Route path="/mylistings" element={<MyListings/>} />
+          <Route path="/:item" element={<ItemPage/>} />
+          <Route path="/*" element={<Home/>} />
+          </Routes>
+          <ToastContainer />
+          </Layout>
+      </BrowserRouter>
+
+  );
+}
+
+export default App;
